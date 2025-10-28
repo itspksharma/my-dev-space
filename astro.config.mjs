69f63608e35d7ec6da518b802680ced3e190aa63
itspksharma <210720100076@cutm.ac.in>
2025-10-28 20:06:30 +0530

    update astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // ✅ correct import
+import tailwind from '@astrojs/tailwind'; 
+import sitemap from '@astrojs/sitemap';
 
 
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.askdevpk.me',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   devOptions: {
     toolbar: false  // Disables Astro's built-in dev toolbar
   }
